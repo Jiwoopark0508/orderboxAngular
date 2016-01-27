cf0896b19155942c9fd34d8a2017a96a143105e4
EC2 Default User <ec2-user@ip-172-31-10-145.ap-northeast-2.compute.internal>
2016-01-27 15:27:55 +0000

    orderbox working fine

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,18 +1,43 @@
 'use strict';
-(function(){
+(function($){
     var orderboxApp = angular.module('orderboxApp', []);
-
+    var basket = [];
+    var total = 0;
     orderboxApp.controller('OrderBoxCtrl', ['$scope', '$http', function($scope, $http){
-    
+
             $http.get('foods/foods.json').success(function(data){
                 $scope.foods = data;
             });
+
+
+            $scope.quantity = 1;
+            $scope.basket = basket;
+            $scope.total = total;
             $scope.orderProp = 'age';
-            $scope.func = function(){
-                console.log("hi");
-            }
+
+            $scope.addBasket = function(_quantity){
+                        var index = $.inArray(this.food, $scope.basket);
+                        // food is in the basket
+                        if( index !== -1){
+                            $scope.basket[index].count += _quantity;
+                        }
+                        // food is not in the basket
+                        else{
+                            this.food.count = _quantity;
+                            $scope.basket.push(this.food);   
+                        }
+                            };
+            $scope.calculate = function(){
+                            $scope.total = 0;
+                            $scope.basket.forEach(function(item){
+                                $scope.total += item.price * item.count;
+                            })
+                        };
+
             
-        }]);    
+
+            
+    }]); 
 
 
-})();
+})(jQuery);
